fix bug in risk calculation

equityboard/app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 from capm import capm, efficient_frontier, profit, stock_prices, daily_return
-from capm import risk
+from capm import risk, _n_year
 from download import us_tickers
 import numpy as np
 from datetime import date
@@ -27,12 +27,12 @@ def capm_scatter(
         df_daily: pd.DataFrame, risk: pd.Series, profit: pd.Series,
         from_day, to_day, tickers: list[str]) -> None:
     df_capm = capm(risk, profit)
+    print(profit.head())
     res = None
 
     if len(tickers) > 1:
         target_profits = np.linspace(max(0., profit.min()),
                                      profit.max(), 10)
-
         res = efficient_frontier(
             df_daily[tickers], profit, target_profits, from_day, to_day)
 
@@ -179,7 +179,10 @@ def update_output(tickers, n_clicks, from_day, to_day):
                             df.index[-1].strftime(iso_fmt))
     df_profit = profit(df, from_day, to_day)
     ser_risk = risk(df_daily, from_day, to_day)
+
     ser_profit = df_profit.iloc[-1]
+    n_year = _n_year(from_day, to_day)
+    ser_profit = ser_profit.abs().pow(1. / n_year) * np.sign(ser_profit)
     ser_profit.name = 'return'
     return [
         chart(df_profit, tickers),

equityboard/capm.py
@@ -24,29 +24,28 @@ def stock_prices(tickers=None):
     return df
 
 
+def _n_year(from_day: str, to_day: str) -> float:
+    return (date.fromisoformat(to_day) - date.fromisoformat(
+        from_day)).days / 365.
+
+
 def risk(
         df_daily: pd.DataFrame, from_day: str, to_day: str,
         annual=True) -> pd.Series:
     r = (df_daily - df_daily.mean(axis=0)).var(axis=0)
     r.name = 'risk'
     if annual:
-        n_year = (date.fromisoformat(to_day) - date.fromisoformat(
-            from_day)).days / 365.
+        n_year = _n_year(from_day, to_day)
         r = r / n_year
     return r
 
 
 def profit(
-        df: pd.DataFrame, from_day: str, to_day: str,
-        annual=True) -> pd.DataFrame:
+        df: pd.DataFrame, from_day: str, to_day: str) -> pd.DataFrame:
     # TODO use lambda
     df_profit = (df[from_day:to_day].interpolate(method='backfill', axis=0)
                  .subtract(df.loc[from_day], axis=1)
                  .divide(df.loc[from_day], axis=1)) * 100.
-    if annual:
-        n_year = (date.fromisoformat(to_day) - date.fromisoformat(
-            from_day)).days / 365.
-        df_profit = df_profit / n_year
     return df_profit[from_day:to_day]
 
 
@@ -81,7 +80,7 @@ def efficient_frontier(
             np.sum(
                 (df.to_numpy() * weights.reshape(1, -1)),
                 axis=1)
-            .var())
+            .var()) / _n_year(from_day, to_day)
 
     n_ticker = len(df_daily.columns)
     if n_ticker == 1: